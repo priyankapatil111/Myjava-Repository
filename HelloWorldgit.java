3c5f5e6c56a7ca45157a3ed96a3f269fad5d6994
priyanka patil <priyanka_patil2@persistent.co.in>
2018-07-26 15:24:58 +0530

    Method calling conflict fix

--- HelloWorldgit.java
+++ HelloWorldgit.java
@@ -1,16 +1,36 @@
+
 Class HelloWorld
 {
 
-	void diplay()
+	static void diplay()
+
+
 	{
 		System.out.println("Todays date");
 
-		Sytem.out.println("hey..!!!!!")
+		System.out.println("hey..!!!!!");
 	}
 
+
+
+
 public static void main(String Args[])
-   {
+{
+
 
 	System.out.println("HEllo World....... Welocome to Git!!!!");
-   }
+<<<<<<< HEAD
+    
+	 
+
+	 HelloWorld ob1=new HelloWorld();
+	 ob1.diaplay();
+
+
+}
+
+=======
+	display();  
+ }
+>>>>>>> bug456
 }
